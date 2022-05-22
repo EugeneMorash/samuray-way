fb298e722e4236e9b0d79f57eed15cea16e3bbcf
EugeneMorash <85665094+EugeneMorash@users.noreply.github.com>
2022-05-22 17:06:06 +0300

    lesson 22 done. Dialogs greed

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,11 +2,34 @@ import React from 'react';
 import styles from './Dialogs.module.css'
 
 
-
 export function Dialogs() {
     return (
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                <div className={styles.dialog + ' ' + styles.active}>
+                    Sveta
+                </div>
+                <div className={styles.dialog}>
+                    Kolya
+                </div>
+                <div className={styles.dialog}>
+                    Lucia
+                </div>
+                <div className={styles.dialog}>
+                    Kostya
+                </div>
+                <div className={styles.dialog}>
+                    Yulia
+                </div>
+                <div className={styles.dialog}>
+                    Sasha
+                </div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>Hi</div>
+                <div className={styles.message}>What's up?</div>
+                <div className={styles.message}>Cool</div>
+            </div>
         </div>
     );
 }
